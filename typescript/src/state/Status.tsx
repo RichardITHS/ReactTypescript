35c1b9a6d4c9a4ed289f3c710bc531f6f610fc3b
Navigoo <navigoo@me.com>
2023-04-04 12:04:15 +0200

    update

--- typescript/src/state/Status.tsx
+++ typescript/src/state/Status.tsx
@@ -4,27 +4,30 @@ type StatusProps = {
     status: string
 }
 
-function Status(props: StatusProps) {
+//Nedan ett steg ytterligare, tvingar typen att det ska endast vara nedan alternativ
+/* type StatusProps = {
+    status: 'loading' | 'success' | 'error'
+} */
 
+function Status(props: StatusProps) {
     let message
 
-    if(props.status === 'loading'){
+    if (props.status === 'loading') {
         message = 'Laddar'
-    }else if(props.status === 'success'){
+    } else if (props.status === 'success') {
         message = 'All data har inhämtats'
-    }else if(props.status === 'error'){
+    } else if (props.status === 'error') {
         message = 'Vi har ett error'
     }
 
-  return (
-    <div>
-       {/*  <h2>Laddar...</h2>
+    return (
+        <div>
+            {/*  <h2>Laddar...</h2>
         <h2>Sidan har gjort en fetch</h2>
         <h2>Error vid fetch</h2> */}
-        <h2>Status - {message}</h2>
-
-    </div>
-  )
+            <h2>Status - {message}</h2>
+        </div>
+    )
 }
 
 export default Status
